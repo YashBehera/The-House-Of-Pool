c899f45407cfb46de7671569cd2f6d8178949529
YashBehera <75005275+YashBehera@users.noreply.github.com>
2025-03-06 17:00:19 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,8 @@ import { MenuOutlined } from "@ant-design/icons";
 import "./Navbar.css";
 import Login from "./Login";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import logo from "./logo.png";
+import logohop from "./image.png";
 
 const { Option } = Select;
 
@@ -214,14 +216,18 @@ const Navbar = ({
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-[#001529] text-white flex items-center justify-between px-4 py-2 z-10">
+    <div className="fixed top-0 left-0 w-full h-[56px] bg-[#001529] text-white flex items-center justify-between px-4 py-2 z-10">
       {/* Title */}
-      <button
-        className="text-xl md:text-2xl font-semibold"
-        onClick={() => navigate("/")}
-      >
-        The House Of Pool
-      </button>
+      <div className="flex gap-4 ">
+        <button
+          className="text-xl md:text-2xl font-semibold"
+          onClick={() => navigate("/")}
+        >
+          The House Of Pool
+        </button>
+        <img src={logo} className="h-[53px] m-[3px] rounded-b-3xl" alt="Logo" />
+        <img src={logohop} className="h-[56px]" alt="LogoHOP" />
+      </div>
 
       {/* Mobile Menu Toggle */}
       <div className="md:hidden">
@@ -261,13 +267,13 @@ const Navbar = ({
         >
           <span className="text-white">Reports</span>
         </Button>
-        <Button
+        {/*         <Button
           type="link"
           className="text-white text-base hover:text-gray-300"
           onClick={() => handleRestrictedAction("expenses")}
         >
           <span className="text-white">Expenses</span>
-        </Button>
+        </Button> */}
         <Button
           type="link"
           className="text-white text-base hover:text-gray-300"
