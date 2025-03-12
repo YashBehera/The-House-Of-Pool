a6a4124fe621be8cd85a2b7ad4028d0d0e115534
YashBehera <75005275+YashBehera@users.noreply.github.com>
2025-03-12 18:08:09 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -294,7 +294,7 @@ const Navbar = ({
           className="text-white text-base hover:text-gray-300"
           onClick={() => navigate(`/queue?location=${getHomeLocation()}`)}
         >
-          <span className="text-white">Waiting</span>
+          <span className="text-white">Booking</span>
         </Button>
         {userRole !== "restricted" || isActionAuthenticated ? (
           <Select
